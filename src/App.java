7482fdb1115bc6bea3bff090db6c5e4abacedb2e
Javier Moreno <jdmoreno26@outlook.com>
2025-05-27 21:34:04 -0500

    feat: subclase que valida la carga máxima estimada según el peso del chasis

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ public class App {
         Motor motor = new Motor("M001", "Bosch", 4, 120);
         Transmision transmision = new Transmision("T001", "ZF", "Automática", 6);
         Neumatico neumatico = new Neumatico("N001", "Michelin", 17, 32.5);
+        Chasis chasis = new Chasis("C001", "Toyota", "Monocasco", 250.0);
 
         System.out.println("==== MOTOR ====");
         motor.mostrarInformacion();
@@ -17,6 +18,10 @@ public class App {
         System.out.println("\n==== NEUMÁTICO ====");
         neumatico.mostrarInformacion();
         neumatico.verificarPresion();
+
+        System.out.println("\n==== CHASIS ====");
+        chasis.mostrarInformacion();
+        chasis.calcularCargaMaxima();
 
     }
 }
