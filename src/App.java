99bf6ea0f4cd2843a482ec03a66d47806185e1b2
Javier Moreno <jdmoreno26@outlook.com>
2025-05-27 20:13:34 -0500

    feat: subclase que imprime mensaje de encendido con potencia y cilindros

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import Models.Motor;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Motor motor = new Motor("M001", "Bosch", 4, 120);
+
+        System.out.println("==== MOTOR ====");
+        motor.mostrarInformacion();
+        motor.encenderMotor();
     }
 }
