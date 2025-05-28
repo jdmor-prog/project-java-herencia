824451eb346de99713a11c0dc4d4620e1fd26fd9
Javier Moreno <jdmoreno26@outlook.com>
2025-05-27 20:24:57 -0500

    feat: subclase que cambia la marcha validando el rango disponible

--- src/App.java
+++ src/App.java
@@ -1,11 +1,16 @@
-import Models.Motor;
+import Models.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Motor motor = new Motor("M001", "Bosch", 4, 120);
+        Transmision transmision = new Transmision("T001", "ZF", "Automática", 6);
 
         System.out.println("==== MOTOR ====");
         motor.mostrarInformacion();
         motor.encenderMotor();
+
+        System.out.println("\n==== TRANSMISIÓN ====");
+        transmision.mostrarInformacion();
+        transmision.cambiarMarcha(3);
     }
 }
